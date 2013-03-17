1f09efb4e44ad8bec88e6fa3457ff5738832c6f9
Steven Smethurst <steven@abluestar.com>
2013-03-16 17:01:38 -0700

    Added LED pins

--- sem/sem.ino
+++ sem/sem.ino
@@ -8,20 +8,32 @@
    private:
      int m_echoPin ;
      int m_trigPin ; 
+     
+     int m_redLedPin ; 
+     int m_greenLedPin ; 
+     int m_blueLedPin ; 
+     
+     int m_servoPin ; 
 
      int   ReadInputs();    
      void  WriteOutputRGB( int value ); 
      
    public:
-     void SetUp( int echo, int trig ); 
+     void SetUp( int echo, int trig, int redLed, int greenLed, int blueLed,  int servoPin  ); 
      void Loop(); 
 
  };
  
-void CSEM::SetUp( int echo, int trig ) {
+void CSEM::SetUp( int echo, int trig, int redLed, int greenLed, int blueLed, int servoPin ) {
   // Record the pins
   this->m_echoPin = echo ; 
   this->m_trigPin = trig ;   
+
+  this->m_redLedPin   = redLed ; 
+  this->m_greenLedPin = greenLed ; 
+  this->m_blueLedPin  = blueLed ; 
+  
+  this->m_servoPin = servoPin ; 
 
   // Set up the serial 
   Serial.begin (9600);
@@ -29,6 +41,10 @@ void CSEM::SetUp( int echo, int trig ) {
   // Set the pins up. 
   pinMode(this->m_trigPin, OUTPUT);
   pinMode(this->m_echoPin, INPUT);
+  
+  pinMode(this->m_redLedPin,   OUTPUT);
+  pinMode(this->m_greenLedPin, OUTPUT);
+  pinMode(this->m_blueLedPin,  OUTPUT);  
   
   // debug, Print pins
   Serial.print("Trig Pin: ");  
@@ -48,11 +64,17 @@ int CSEM::ReadInputs() {
   digitalWrite(this->m_trigPin, HIGH);
   delayMicroseconds(10);   
   digitalWrite(this->m_trigPin, LOW);
-    
+
+  /** 
+   * Instead of using the Speed of Sound, we can also use the "Pace of Sound".
+   * The Pace of Sound = 1 / Speed of Sound = 1 / 0.03435 = 29.1 ųs/cm
+   * In this case the equation to calculate the distance become: D = (Δt/2) / Pace of sound
+   * and for the example above: D = 250 / 29.1 = 8.6 cm   
+   */    
   duration = pulseIn(this->m_echoPin, HIGH);
   distance = (duration/2) / 29.1;
 
-  
+  // This sensors has a max of 200 cm 
   if (distance >= 200 || distance <= 0){
     Serial.println("Out of range");
     return 0; 
@@ -65,6 +87,11 @@ int CSEM::ReadInputs() {
 }
 
 void CSEM::WriteOutputRGB( int value ) {
+/*     int m_redLedPin ; 
+     int m_greenLedPin ; 
+     int m_blueLedPin ; 
+*/
+
   if( value <= 10 ) {
     Serial.println(" Red ");    
   } else if( value > 10 && value <= 20  ) {
@@ -90,9 +117,16 @@ void CSEM::Loop() {
 #define trigPin 13
 #define echoPin 12
 
+#define redPin    11
+#define greenPin  10
+#define bluePin    9
+
+#define servoPin   8
+
+
 CSEM g_SDM; 
 void setup() {
-  g_SDM.SetUp( echoPin, trigPin ); 
+  g_SDM.SetUp( echoPin, trigPin, redPin, greenPin, bluePin, servoPin ); 
 }
 void loop() {
   g_SDM.Loop();

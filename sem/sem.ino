2c2f3e1406127017243c4ad9a2c6eb2fb25ff802
Steven Smethurst <git@funvill.com>
2013-03-18 14:46:42 -0700

    Updated the LED for common cathod

--- sem/sem.ino
+++ sem/sem.ino
@@ -2,9 +2,6 @@
  * Vancouver Maker Faire 2013
  * 
  */
-
-#include <Servo.h> 
-
  
  class CSEM 
  {
@@ -16,28 +13,28 @@
      int m_greenLedPin ; 
      int m_blueLedPin ; 
      
-     Servo m_servo ; 
-
+     int m_servoPin ; 
+ 
      int   ReadInputs();    
      void  WriteOutputRGB( int value ); 
      
    public:
      void SetUp( int echo, int trig, int redLed, int greenLed, int blueLed,  int servoPin  ); 
      void Loop(); 
-
+ 
  };
  
 void CSEM::SetUp( int echo, int trig, int redLed, int greenLed, int blueLed, int servoPin ) {
   // Record the pins
   this->m_echoPin = echo ; 
   this->m_trigPin = trig ;   
-
+ 
   this->m_redLedPin   = redLed ; 
   this->m_greenLedPin = greenLed ; 
   this->m_blueLedPin  = blueLed ; 
   
-  this->m_servo.attach( servoPin ) ; 
-
+  this->m_servoPin = servoPin ; 
+ 
   // Set up the serial 
   Serial.begin (9600);
   
@@ -67,7 +64,7 @@ int CSEM::ReadInputs() {
   digitalWrite(this->m_trigPin, HIGH);
   delayMicroseconds(10);   
   digitalWrite(this->m_trigPin, LOW);
-
+ 
   /** 
    * Instead of using the Speed of Sound, we can also use the "Pace of Sound".
    * The Pace of Sound = 1 / Speed of Sound = 1 / 0.03435 = 29.1 ųs/cm
@@ -76,7 +73,7 @@ int CSEM::ReadInputs() {
    */    
   duration = pulseIn(this->m_echoPin, HIGH);
   distance = (duration/2) / 29.1;
-
+ 
   // This sensors has a max of 200 cm 
   if (distance >= 200 || distance <= 0){
     Serial.println("Out of range");
@@ -88,48 +85,60 @@ int CSEM::ReadInputs() {
   Serial.println(" cm");
   return distance ; 
 }
-
+ 
 void CSEM::WriteOutputRGB( int value ) {
-/*     int m_redLedPin ; 
-     int m_greenLedPin ; 
-     int m_blueLedPin ; 
-*/
-
-  if( value <= 10 ) {
+ 
+  int red   = 255 ; 
+  int green = 255 ; 
+  int blue  = 255 ; 
+  
+  if( value <= 20 ) {
     Serial.println(" Red ");    
-  } else if( value > 10 && value <= 20  ) {
-    Serial.println(" Blue ");    
+    red = 0 ; 
   } else if( value > 20 && value <= 50  ) {
+    Serial.println(" Yellow ");    
+    red   = 0 ; 
+    green = 0 ; 
+  } else if( value > 50  ) {
     Serial.println(" green ");    
+    green = 0 ; 
   }
+  
+  analogWrite(this->m_redLedPin,   red);
+  analogWrite(this->m_blueLedPin,  blue);
+  analogWrite(this->m_greenLedPin, green);
+ 
 }
  
-
+ 
 void CSEM::Loop() {
-  int ret = this->ReadInputs() ; 
-  this->WriteOutputRGB( ret ); 
-
-  int servo = ret;
-  if (servo == 0) // out of range
-    servo = 180;
-  this->m_servo.write(90 - map(servo, 0, 180, 0, 90));
   
-  delay( 50 ); 
+  // Create an average betten several sensors. 
+  int total = 0 ; 
+  int count = 0 ; 
+  for( ; count < 5; count ++ ) {
+    total += this->ReadInputs() ; 
+  }
+  int value = total / count ;   
+ 
+  // Write the value out to the outputs. 
+  this->WriteOutputRGB( value ); 
+  
 }
-
-
-
-
+ 
+ 
+ 
+ 
 #define trigPin 13
 #define echoPin 12
-
-#define redPin    11
+ 
+#define redPin     8
 #define greenPin  10
 #define bluePin    9
-
+ 
 #define servoPin   14
-
-
+ 
+ 
 CSEM g_SDM; 
 void setup() {
   g_SDM.SetUp( echoPin, trigPin, redPin, greenPin, bluePin, servoPin ); 
